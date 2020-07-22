ca55c2c34278a610453f34e103e56671adb5189c
Rain72 <rain72@gmail.com>
2020-07-22 09:57:31 +0800

    [mod] refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,104 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { VcprojFileTreeDataProvider, VcprojViewItem } from './vcprojTreeDataProvider';
-import * as Path from 'path';
-
-let vcprojExplorer: vscode.TreeView<VcprojViewItem> = null;
-let vcprojTreeDataProvider: VcprojFileTreeDataProvider = null;
-let nextViewItemFocus: boolean = false;
+import {VcprojExplorer} from './vcprojExplorer'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	if (vscode.workspace.workspaceFolders == null 
-		|| vscode.workspace.workspaceFolders.length === 0) {
-        return;
-	}
-	vscode.commands.executeCommand("setContext", "vcprojView.enable", true);
+	if(!VcprojExplorer.checkWorkspaceFolders())
+		return;
 	
-	const rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-
-	const vcprojPathEventEmitter = new vscode.EventEmitter<string>();
-	vcprojPathEventEmitter.event(async (vcprojPath) => {
-		if (vcprojPath.length <= 0) {
-			return;
-		}
-		if (vcprojExplorer) {
-			vcprojExplorer.dispose();
-		}
-		const path = Path.resolve(rootPath, vcprojPath);
-		vcprojExplorer = vscode.window.createTreeView("vcprojView",
-            {
-                treeDataProvider: (vcprojTreeDataProvider = new VcprojFileTreeDataProvider(path)),
-                showCollapseAll: true,
-                canSelectMany: false,
-            }
-        );
-		vcprojExplorer.title += `(${Path.basename(path)})`;
-		vcprojExplorer.onDidChangeVisibility((e) => {
-			setViewItemSelected();
-		});
-	});
-
-	vscode.commands.registerCommand('vcprojExplorer.openVcproj', async () => {
-		const uri = await vscode.window.showOpenDialog({
-			openLabel: "Open .vcproj",
-			defaultUri: vscode.workspace.workspaceFolders[0].uri,
-			canSelectFiles: false,
-			filters: {
-				vcproj: ['vcproj']
-			}
-		});
-		
-		if(!uri || uri.length <= 0) 
-			return;
-		const path = Path.relative(rootPath, uri[0].fsPath);
-		await vscode.workspace.getConfiguration("vcprojexplorer").update('file', path);
-		vcprojPathEventEmitter.fire(path);
-	});
-	vscode.commands.registerCommand('vcprojExplorer.openFile', async (resource) => {
-		let doc = await vscode.workspace.openTextDocument(resource); // calls back into the provider
-		nextViewItemFocus = true;
-		vscode.window.showTextDocument(doc);
-	});
-	vscode.commands.registerCommand("vcprojExplorer.goInto",
-		(value: VcprojViewItem) => {
-			if (value == null || !value.filter) {
-				return;
-			}
-			vcprojTreeDataProvider.goInto(value.label, value.filter);
-			vscode.commands.executeCommand("setContext", "vcprojView.goInto", true);
-			setViewItemSelected();
-		}
-	);
-	vscode.commands.registerCommand("vcprojExplorer.goHome",
-		() => {
-			vcprojTreeDataProvider.goHome();
-			vscode.commands.executeCommand("setContext", "vcprojView.goInto", false);
-			setViewItemSelected();
-		}
-	);
-
-	vscode.window.onDidChangeActiveTextEditor( async (e) => {
-		setViewItemSelected(e.document.fileName);
-	});
-
-	vcprojPathEventEmitter.fire(vscode.workspace.getConfiguration("vcprojexplorer").get('file'));
+	VcprojExplorer.init();
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
 
-async function setViewItemSelected(fileUri?: string) : Promise<void> {
-	let focus = nextViewItemFocus;
-	nextViewItemFocus = false;
-	if (!vcprojTreeDataProvider || !vcprojExplorer?.visible)
-		return;
-	let viewItem = await vcprojTreeDataProvider.find(fileUri || vscode.window.activeTextEditor.document.fileName);
-	if (!viewItem)
-		return;
-	vcprojExplorer.reveal(viewItem, { focus });
-}
+
---
+++ src/vcprojExplorer.ts
@@ -0,0 +1,106 @@
+import * as vscode from 'vscode';
+import * as Path from 'path';
+import { VcprojFileTreeDataProvider, VcprojViewItem } from './vcprojTreeDataProvider';
+
+export class VcprojExplorer {
+    static rootPath: string = '';
+    static vcprojPathEventEmitter = new vscode.EventEmitter<string>();
+    static vcprojExplorer: vscode.TreeView<VcprojViewItem> = null;
+    static vcprojTreeDataProvider: VcprojFileTreeDataProvider = null;
+
+    public static checkWorkspaceFolders(): boolean {
+        return vscode.workspace.workspaceFolders != null 
+            && vscode.workspace.workspaceFolders.length > 0;
+    }
+
+    public static init(): void {
+        this.rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+        vscode.commands.registerCommand('this.openVcproj', async () => {
+            await this.cmdSetVcprojPath();
+            this.openVcropj();
+        });
+        vscode.commands.registerCommand('vcprojExplorer.openFile', this.cmdOpenFile.bind(this));
+        vscode.commands.registerCommand("vcprojExplorer.goInto", this.goInto.bind(this));
+        vscode.commands.registerCommand("vcprojExplorer.goHome", this.goHome.bind(this));
+
+        vscode.window.onDidChangeActiveTextEditor((e) => { this.setViewItemSelected(e.document.fileName); });
+
+        this.openVcropj();
+        
+        vscode.commands.executeCommand("setContext", "vcprojView.enable", true);
+    }
+
+    private static async cmdSetVcprojPath(): Promise<void> {
+        const uri = await vscode.window.showOpenDialog({
+            openLabel: "Open .vcproj",
+            defaultUri: vscode.workspace.workspaceFolders[0].uri,
+            canSelectFiles: false,
+            filters: {
+                vcproj: ['vcproj']
+            }
+        });
+        
+        if(!uri || uri.length <= 0) 
+            return;
+        const path = Path.relative(this.rootPath, uri[0].fsPath);
+        await vscode.workspace.getConfiguration("vcprojexplorer").update('file', path);
+    }
+
+    private static async cmdOpenFile(resource: string): Promise<void> {
+        vscode.window.showTextDocument(
+            await vscode.workspace.openTextDocument(resource), 
+            { preserveFocus: true }
+        );
+    }
+
+    private static async openVcropj(): Promise<void> {
+        let vcprojPath: string = vscode.workspace.getConfiguration("vcprojexplorer").get('file');
+        if (vcprojPath.length <= 0) {
+            return;
+        }
+        if (this.vcprojExplorer) {
+            this.vcprojExplorer.dispose();
+        }
+        const path = Path.resolve(this.rootPath, vcprojPath);
+        this.vcprojExplorer = vscode.window.createTreeView("vcprojView",
+            {
+                treeDataProvider: (this.vcprojTreeDataProvider = new VcprojFileTreeDataProvider(path)),
+                showCollapseAll: true,
+                canSelectMany: false,
+            }
+        );
+        this.vcprojExplorer.title += `(${Path.basename(path)})`;
+        this.vcprojExplorer.onDidChangeVisibility((e) => {
+            this.setViewItemSelected();
+        });
+    }
+
+    private static async setViewItemSelected(fileUri?: string) : Promise<void> {
+        if (!this.vcprojTreeDataProvider || !this.vcprojExplorer?.visible)
+            return;
+        let viewItem = await this.vcprojTreeDataProvider.find(fileUri || vscode.window.activeTextEditor.document.fileName);
+        if (!viewItem)
+            return;
+        this.vcprojExplorer.reveal(viewItem);
+    }
+
+    private static goInto(value: VcprojViewItem): void {
+        if (value == null || !value.filter) {
+            return;
+        }
+        this.vcprojTreeDataProvider.goInto(value.label, value.filter);
+        vscode.commands.executeCommand("setContext", "vcprojView.goInto", true);
+        this.setViewItemSelected();
+    }
+
+    private static goHome(value: VcprojViewItem): void {
+        this.vcprojTreeDataProvider.goHome();
+        vscode.commands.executeCommand("setContext", "vcprojView.goInto", false);
+        this.setViewItemSelected();
+    }
+
+}
+
+
+export default this;
